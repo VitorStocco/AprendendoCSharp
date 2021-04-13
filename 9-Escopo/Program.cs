3c0294c82f58b7fc311dfcf7d078b5e43b9f527f
VitorStocco <vitor.stocco.garcia@gmail.com>
2021-04-13 17:16:22 -0300

    Aula

---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 8 - Condicionais 2");
+            int idade = 16;
+            bool acompanhado = true;
+            string mensagemAdicional;
+          
+            if (acompanhado == true)
+            {
+              mensagemAdicional = "Esta Acompanhado";
+            }
+            else
+              mensagemAdicional = "Esta Sozinho";
+
+            // No else se ele tiver uma linha só posso tirar { }
+
+            if (idade >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode Entar!!!");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+                Console.WriteLine("Não pode entrar!!!");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicios2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 5000.00;
+
+            if (salario >= 1900.00 && salario <= 2800.00)
+            {
+                Console.WriteLine("O Seu Salario é " + salario + ", O IR é de 7.5% e pode deduzir na declaração o valor de R$ 142;");
+            }
+            else if (salario >= 2800.01 && salario <= 3751.00)
+            {
+                Console.WriteLine("O Seu Salario é " + salario + ", O IR é de 15% e pode deduzir na declaração o valor de R$ 350;");
+            }
+            else if (salario >= 3751.01 && salario <= 4664.00)
+            {
+                Console.WriteLine("O Seu Salario é " + salario + ", O IR é de 22.5% e pode deduzir na declaração o valor de R$ 636");
+            }
+            else
+                Console.WriteLine(salario);
+
+
+            Console.ReadLine();
+        }
+    }
+}
