1adb9c95d80b4058885c6b6684b77f5706c39d79
VitorStocco <vitor.stocco.garcia@gmail.com>
2021-04-15 16:00:29 -0300

    Aula

---
+++ 10-CalculaPoupanca/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Calcula Poupança!");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+           while(contadorMes <= 12)
+
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes +" Mês, Você terá " + valorInvestido);
+
+                //contadorMes = contadorMes + 1;]
+                //contadorMes += 1;
+                contadorMes++;
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
