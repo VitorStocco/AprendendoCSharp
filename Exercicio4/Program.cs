ea280267106568dbabfe4f9502d1fe1911675453
VitorStocco <vitor.stocco.garcia@gmail.com>
2021-04-16 18:02:58 -0300

    Exercicios

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Programa
+{
+    static void Main(String[] args)
+    {
+        for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+         {
+             for (int contador = 0; contador <= 10; contador++)
+             {
+                 Console.Write(multiplicador + " * " + contador + " = " + multiplicador * contador);
+                 Console.WriteLine();
+             }
+            Console.WriteLine();
+        }
+         Console.ReadLine();
+
+        /*
+
+        for (int linha = 0; linha < 10; linha++)
+        {
+            for (int coluna = 0; coluna < 10; coluna++)
+            {
+                if (coluna > linha)
+                {
+                    break;
+                }
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();  */
+    }
+}
